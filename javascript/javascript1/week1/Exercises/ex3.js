fa270fda619053371359f291b2c38747fedebc1c
furkanozbek <furkannozbek@yandex.com>
2024-04-21 17:56:23 +0200

    Some exercises

---
+++ javascript/javascript1/week1/Exercises/ex3.js
@@ -0,0 +1,9 @@
+let userName = "benjamin";
+userName = "benjamin-better";
+
+const pizzaPrice = 78;
+const pizzaPriceDiscounted = pizzaPrice - 10;
+
+const users = ["peter", "Johnny", "Børge"];
+
+const lastUser = users[3]
