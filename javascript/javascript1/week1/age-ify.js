dcecfea997b914e1ae641725fa49092f2e088ed8
furkanozbek <furkannozbek@yandex.com>
2024-04-24 21:12:23 +0200

    Changed template strings for better read

--- javascript/javascript1/week1/age-ify.js
+++ javascript/javascript1/week1/age-ify.js
@@ -4,4 +4,4 @@ const yearFuture = 2078;
 // Calculation of age with given variables
 const age = yearFuture - yearOfBirth;
 // Logging the result to the console
-console.log("You will be " + age + " years old in " + yearFuture);
+console.log(`You will be ${age} years old in ${yearFuture}`);
