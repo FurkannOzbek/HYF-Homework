845b59ab61297dfd557b7a2d7277e24ad7fe3ca1
furkanozbek <furkannozbek@yandex.com>
2024-05-02 01:10:16 +0200

    camelCase fix

--- javascript/javascript1/week2/event-application.js
+++ javascript/javascript1/week2/event-application.js
@@ -1,7 +1,7 @@
 // Event application
 
 // Created a function for calculating the day for an event creation
-function getEventWeekday(dayslater) {
+function getEventWeekday(daysLater) {
   // Fetching the system date put into a variable
   const date = new Date();
   // Created an array for the day names because we get the day from system as a number not as a name
@@ -17,11 +17,10 @@ function getEventWeekday(dayslater) {
   // Added variable for current day name
   const currentDay = daysOfWeek[date.getDay()];
   // Added variable for calculating which day the events will be held
-  const eventDay = daysOfWeek[(date.getDay() + dayslater) % 7];
+  const eventDay = daysOfWeek[(date.getDay() + daysLater) % 7];
   //Logging out current day and event day as text
   console.log(`Today is ${currentDay}`);
   console.log(`Planned event will be on ${eventDay}`);
 }
 
-// Calling the function created with parameter that I spesified
 getEventWeekday(36);
