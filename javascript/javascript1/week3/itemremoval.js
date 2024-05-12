d19610948cc96d46dbb16e88d793ba8d1729d855
furkanozbek <furkannozbek@yandex.com>
2024-05-12 22:24:09 +0200

    add a break point for the loop

--- javascript/javascript1/week3/itemremoval.js
+++ javascript/javascript1/week3/itemremoval.js
@@ -14,6 +14,7 @@ const nameToRemove = "Tala"; //This is adjustable to see if console.logs will be
 for (i = 0; i < names.length; i++) {
   if (names[i] === nameToRemove) {
     names.splice(i, 1);
+    break;
   }
 }
 // Code done
