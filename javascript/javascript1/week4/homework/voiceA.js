2a8419da154c0edeb33c15513ce3737c57facd77
FurkannOzbek <furkannozbek@yandex.com>
2024-05-17 18:26:05 +0200

    dont know what i added

--- javascript/javascript1/week4/homework/voiceA.js
+++ javascript/javascript1/week4/homework/voiceA.js
@@ -9,7 +9,7 @@ const patterns = {
 };
 const operations = {
   plus: (num1, num2) => num1 + num2,
-  minus: (num1, num2) => num1 - num2, // Sometimes bot get it as string instead of symbols so i had to add double
+  minus: (num1, num2) => num1 - num2, // Sometimes chatbot get it as string instead of symbols so i had to add double
   multiple: (num1, num2) => num1 * num2,
   divide: (num1, num2) => num1 / num2,
   "+": (num1, num2) => num1 + num2,
