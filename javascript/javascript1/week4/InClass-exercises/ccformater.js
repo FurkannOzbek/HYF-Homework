e0ecd2eb424038cb7f72cfea7843b5b1eaef9f48
FurkannOzbek <furkannozbek@yandex.com>
2024-05-17 19:38:03 +0200

    code fix

--- javascript/javascript1/week4/InClass-exercises/ccformater.js
+++ javascript/javascript1/week4/InClass-exercises/ccformater.js
@@ -2,31 +2,26 @@
 
 // check for 16 digits
 
-let k = 0;
-
 function formatCreditCardNumber(number) {
-  if (typeof number !== "number") {
+  if (typeof number !== "number" && typeof number !== "string") {
     console.log("Please type numbers");
   } else {
-    const numbersIntoArray = String(number).split("");
+    const numStr = String(number);
     let str = "";
-    for (i = 0; i < numbersIntoArray.length; i++) {
-      console.log(i);
-      if (i % 4 === 0 && i !== 0) {
-        //   numbersIntoArray.splice(numbersIntoArray[i + k], 0, " ");
-        str += " " + numbersIntoArray[i];
-        // k = k + 1;
-      } else {
-        str += numbersIntoArray[i];
+    let count = 0;
+    for (const char of numStr) {
+      str += char;
+      count++;
+      // Add a space after every fourth character, except after the last character
+      if (count % 4 === 0 && count !== numStr.length) {
+        str += " ";
       }
     }
     return str;
   }
-  //numbersIntoArray = numbersIntoArray.join("");
-  //   return numbersIntoArray;
 }
 
-const formattedCreditCardObject = formatCreditCardNumber(123456789412423);
+const formattedCreditCardObject = formatCreditCardNumber(2567721314124123123);
 console.log(formattedCreditCardObject);
 /*
 {
