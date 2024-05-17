674e68805e174750b611678254d7f5ce52c0d57d
FurkannOzbek <furkannozbek@yandex.com>
2024-05-17 18:24:15 +0200

    Used regex, code fix

--- javascript/javascript1/week4/homework/voiceA.js
+++ javascript/javascript1/week4/homework/voiceA.js
@@ -1,74 +1,54 @@
-const patternNameCheck = "hello my name is ";
-const patternNameRecog = "what is my name";
-const patternToDoAdd = "add";
-const patternToDo = "to my todo";
-const patternToDoRem = "remove";
-const patternFromToDo = "from my todo";
-const patternMyToDo = "my to-do list";
-const patternDayCheck = "what day is it today";
-const patternMathCal = "what is";
+const patterns = {
+  nameCheckRegex: /Hello my name is (.+)/i,
+  nameRecogRegex: /What is my name/i,
+  toDoAddRegex: /Add (.+) to my to/i,
+  toDoRemRegex: /Remove (.+) from my to/i, // I cut them short with purpose because sometimes when u say todo voice asistant either take it as "to do" or "todo"
+  toDoListRegex: /What is on my list/i,
+  dateCheckRegex: /What day is it today/i,
+  mathCheckRegex: /What is (.+) (.+) (.+)/i,
+};
+const operations = {
+  plus: (num1, num2) => num1 + num2,
+  minus: (num1, num2) => num1 - num2, // Sometimes bot get it as string instead of symbols so i had to add double
+  multiple: (num1, num2) => num1 * num2,
+  divide: (num1, num2) => num1 / num2,
+  "+": (num1, num2) => num1 + num2,
+  "-": (num1, num2) => num1 - num2,
+  "*": (num1, num2) => num1 * num2,
+  "/": (num1, num2) => num1 / num2,
+};
 
-let result;
 let nameSave = "";
 let todos = [];
 function getReply(command) {
-  const lowerCaseInput = command.toLowerCase();
-
-  //   const match = lowerCaseInput.match(patternNameCheck
-  const startIndexFirst = lowerCaseInput.indexOf(patternNameCheck); //   console.log(match);
-  const startIndexThird = lowerCaseInput.indexOf(patternToDoAdd);
-  const startIndexFourth = lowerCaseInput.indexOf(patternToDoRem);
-  const startIndexSeventh = lowerCaseInput.indexOf(patternMathCal);
-  if (lowerCaseInput.includes(patternNameCheck)) {
-    if (startIndexFirst !== -1) {
-      const name = command
-        .slice(startIndexFirst + patternNameCheck.length, command.length)
-        .trim();
-
-      if (nameSave.toLowerCase() === name.toLowerCase()) {
-        return `Nice to see you again ${nameSave}`;
-      } else {
-        nameSave = name;
-        return `Nice to meet you ${name}`;
-      }
+  if (command.match(patterns.nameCheckRegex)) {
+    let name = command.match(patterns.nameCheckRegex)[1];
+    if (nameSave === name) {
+      return `Nice to see you again ${nameSave}`;
+    } else {
+      nameSave = name;
+      return `Nice to meet you ${name}`;
     }
-  }
-  if (lowerCaseInput.includes(patternNameRecog)) {
+  } else if (command.match(patterns.nameRecogRegex)) {
     if (nameSave === "") {
       return "You havent put any name";
     } else {
       return `Your name is ${nameSave}`;
     }
-  }
-  if (lowerCaseInput.includes(patternToDoAdd)) {
-    let listItem = command.slice(
-      startIndexThird + patternToDoAdd.length + 1,
-      command.length - patternToDo.length - 1
-    );
+  } else if (command.match(patterns.toDoAddRegex)) {
+    let listItem = command.match(patterns.toDoAddRegex)[1];
     listItem = listItem.charAt(0).toUpperCase() + listItem.slice(1);
     todos.push(listItem);
-    return `${listItem}added to the to-do`;
-  }
-  if (lowerCaseInput.includes(patternToDoRem)) {
-    let removeItem = command.slice(
-      startIndexFourth + patternToDoRem.length + 1,
-      command.length - patternFromToDo.length - 1
-    );
+    return `${listItem} added to the to-do`;
+  } else if (command.match(patterns.toDoRemRegex)) {
+    let removeItem = command.match(patterns.toDoRemRegex)[1];
+
     removeItem = removeItem.charAt(0).toUpperCase() + removeItem.slice(1);
-    let removeIndex = todos.indexOf(removeItem);
-    todos.splice(removeIndex, 1);
-    return `${removeItem}removed from the todo`;
-  }
-  if (lowerCaseInput.includes(patternMyToDo)) {
-    console.log(todos);
-    let toDoList = document.getElementById("to-do");
-    for (var i = 0; i < todos.length; i++) {
-      toDoList.innerHTML += todos[i];
-    }
-    let strTodos = JSON.stringify(todos);
-    return strTodos;
-  }
-  if (lowerCaseInput.includes(patternDayCheck)) {
+    todos.splice(todos.indexOf(removeItem), 1);
+    return `${removeItem} removed from the todo`;
+  } else if (command.match(patterns.toDoListRegex)) {
+    return JSON.stringify(todos);
+  } else if (command.match(patterns.dateCheckRegex)) {
     let date = new Date();
     let options = {
       year: "numeric",
@@ -76,48 +56,47 @@ function getReply(command) {
       day: "numeric",
     };
     return date.toLocaleDateString("en-GB", options);
-  }
-  if (lowerCaseInput.includes(patternMathCal)) {
-    let remainingText = command.slice(
-      startIndexSeventh + patternMathCal.length + 1,
-      command.length
-    );
-    remainingText = remainingText.split(" ");
-    // console.log(remainingText);
-    if (remainingText[1] == "+" || remainingText[1] == "plus") {
-      result = Number(remainingText[0]) + Number(remainingText[2]);
-      result = result.toString();
-      return result;
-    } else if (remainingText[1] == "*" || remainingText[1] == "multiple") {
-      result = Number(remainingText[0]) * Number(remainingText[2]);
-      result = result.toString();
-      return result;
-    } else if (remainingText[1] == "/" || remainingText[1] == "divide") {
-      result = Number(remainingText[0]) / Number(remainingText[2]);
-      result = result.toString();
-      return result;
-    } else if (remainingText[1] == "-" || remainingText[1] == "minus") {
-      result = Number(remainingText[0]) - Number(remainingText[2]);
-      result = result.toString();
-      return result;
+  } else if (command.match(patterns.mathCheckRegex)) {
+    let match = command.match(patterns.mathCheckRegex);
+
+    // console.log(match);
+    // let result = match(operations, num1, num2);
+    // return result;
+
+    const operator = match[2];
+    const num1 = Number(match[1]);
+    const num2 = Number(match[3]);
+
+    if (operations[operator]) {
+      const result = operations[operator](num1, num2);
+      return result.toString(); // Got rid of long if statements
     }
+  } else {
+    return `I couldn't understand your command, please repeat it`;
   }
-  // Alert function with delay for homework
-  //   function timerAlert() {
-  //      alert("Time up");
-  //   }
-  //   setInterval(timerAlert, 4000);
-  //  timerAlert();
 }
 
-// getReply("");
-// getReply("Hello my name is Benjamin");
-// getReply("asd Hello my name is Benjamin");
-// getReply("what is my name");
-// getReply("add ashellowd to my todo");
-// getReply("add fishing to my todo");
-// getReply("remove ashellowd from my todo ");
-// getReply("What is on my todo");
-// getReply("What day is it today");
+// Alert function with delay for homework
+//   function timerAlert() {
+//      alert("Time up");
+//   }
+//   setInterval(timerAlert, 4000);
+//  timerAlert();
 
- console.log(getReply("What is 3 + 5"));
+// getReply("");
+// console.log(getReply("Hello my name is Benjamin"));
+//   console.log(getReply("Hello my name is Benjamin"));
+//   getReply("asd Hello my name is Benjamin");
+// console.log(getReply("what is my name"));
+// console.log(getReply("add ashellowd to my todo"));
+// console.log(getReply("add fishing to my to do"));
+
+// console.log(getReply("remove ashellowd from my todo "));
+// console.log(getReply("What is on my list"));
+// console.log(getReply("What day is it today"));
+// console.log(todos);
+
+// console.log(getReply("What is 3 + 5"));
+// console.log(getReply("What is 3 - 5"));
+// console.log(getReply("What is 3 / 5"));
+// console.log(getReply("What is 3 plus 5"));
