fa75cc416de33408a77003c1320d7c8926f85598
furkanozbek <furkannozbek@yandex.com>
2024-07-16 14:14:21 +0200

    added missing tasks

--- databases/week2/homework2-part4.sql
+++ databases/week2/homework2-part4.sql
@@ -40,13 +40,37 @@ INSERT INTO customer (name,address,phone_number) VALUES
 ("Elena Mainfold", "Albani torv 27 Odense",39393939),
 ("Maria Fox","Tornbjergvej 13 Odense",13131313)
 
+
+-- I added brand which I forgot
 ALTER TABLE car
 ADD COLUMN brand VARCHAR(255) NOT NULL
 
+-- I Was not able to add 4 digit years because I have set it up for tiny int so I have change it into small int
 ALTER TABLE car
-ALTER COLUMN model VARCHAR(255) NOT NULL
+MODIFY COLUMN year SMALLINT NOT NULL
+-- Wrong datatype correction
+ALTER TABLE car
+MODIFY COLUMN model VARCHAR(255) NOT NULL
 
-INSERT INTO car (model,year,price) VALUES
-(2017)
+ALTER TABLE car
+MODIFY COLUMN price BIGINT NOT NULL
 
+INSERT INTO car (brand,model,year,price) VALUES
+("Suzuki","Swift",2019,160000),
+("Ford","Focus",2020,175000),
+("Mercedes","AMG",2024,395000)
+
+
+INSERT INTO salesman(name,phone_number,email) VALUES
+("John Catcher",21212121,"johnca@carsales.com"),
+("Frida Nomskol",14141414,"fridano@carsales.com")
+
+INSERT INTO sale(sale_date,sale_price,customer_id,car_id,salesman_id) VALUES
+("2024-01-14 10:30:00",155000,1,7,1)
+
+-- I gave auto incr for all primary keys but I dont understand why sale_id starts from 2 and car_id starts from 7 , I would be happy to get comment about it thank you.
 select * from sale
+
+select * from sale JOIN car ON sale.car_id = car.car_id
+
+GRANT ALL PRIVILEGES ON mysql.* to 'larec'@'localhost';
