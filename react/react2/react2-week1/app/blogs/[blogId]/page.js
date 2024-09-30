e5fc7ba4f579f59efb0657d675b8412b4edc1de2
FurkannOzbek <furkannozbek@yandex.com>
2024-09-30 14:09:42 +0200

    small performance update

--- react/react2/react2-week1/app/blogs/[blogId]/page.js
+++ react/react2/react2-week1/app/blogs/[blogId]/page.js
@@ -1,13 +1,13 @@
 "use client";
 import { usePathname } from "next/navigation";
 
+const formatWeWant = (word) => {
+  return word.replace(/-/g, " ").replace(/\b\w/g, (char) => char.toUpperCase()); // Capitalize the first letter of each word;
+};
+
 export default function Blog() {
   const path = usePathname();
   const lastPartofPath = path?.split("/").pop();
-  const formatWeWant = (word) => {
-    return word
-      .replace(/-/g, " ")
-      .replace(/\b\w/g, (char) => char.toUpperCase()); // Capitalize the first letter of each word;
-  };
+
   return <div> {formatWeWant(lastPartofPath)}</div>;
 }
