0bfcbe7c8a415ec876b4a7a859e00ca9d7c36999
FurkannOzbek <furkannozbek@yandex.com>
2024-09-30 15:14:00 +0200

    Form validation logic added, after submit redirection added, event listener added

--- react/react2/react2-week1-ex2/app/form/page.jsx
+++ react/react2/react2-week1-ex2/app/form/page.jsx
@@ -1,25 +1,65 @@
 "use client";
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
 import TextField from "@mui/material/TextField";
 import Button from "@mui/material/Button";
+import { useRouter } from "next/navigation";
 
 export default function Form() {
-  // Create refs for each input field
   const firstNameRef = useRef(null);
   const lastNameRef = useRef(null);
   const emailRef = useRef(null);
   const phoneRef = useRef(null);
+  const router = useRouter();
 
-  // Function to handle key press and move focus to the next field
+  const [errorEmail, setErrorEmail] = useState(false);
+  const [errorPhone, setErrorPhone] = useState(false);
+  const [emailHelperText, setEmailHelperText] = useState("");
+  const [phoneHelperText, setPhoneHelperText] = useState("");
+
   const handleKeyPress = (e, nextRef) => {
     if (e.key === "Enter") {
-      // Focus on the next input field if Enter is pressed
       nextRef.current.focus();
     }
   };
 
+  const formSubmit = (e) => {
+    e.preventDefault();
+    const emailValue = emailRef.current.value;
+
+    if (isValidEmail(emailValue)) {
+      setErrorEmail(false);
+      setEmailHelperText("");
+    } else {
+      setErrorEmail(true);
+      setEmailHelperText("Invalid email format");
+      return; // stop the submitting if we get error on format
+    }
+
+    const phoneValue = phoneRef.current.value;
+    if (isValidPhone(phoneValue)) {
+      setErrorPhone(false);
+      setPhoneHelperText("");
+    } else {
+      setErrorPhone(true);
+      setPhoneHelperText("Invalid phone number");
+      return; // same logic as above
+    }
+
+    router.push("/"); // if everything is okay redirect to homepage
+    console.log(emailRef.current.value);
+  };
+
+  function isValidEmail(email) {
+    return email.includes("@");
+  }
+  function isValidPhone(phone) {
+    const phonePattern = /^\d+$/;
+    return phonePattern.test(phone);
+  }
+
   return (
-    <div
+    <form
+      onSubmit={formSubmit}
       style={{
         display: "flex",
         flexDirection: "column",
@@ -30,6 +70,7 @@ export default function Form() {
     >
       {/* First Name Input */}
       <TextField
+        required
         sx={{ marginTop: "2rem" }}
         label="First Name"
         variant="outlined"
@@ -39,6 +80,7 @@ export default function Form() {
 
       {/* Last Name Input */}
       <TextField
+        required
         label="Last Name"
         variant="outlined"
         inputRef={lastNameRef}
@@ -47,20 +89,29 @@ export default function Form() {
 
       {/* Email Input */}
       <TextField
+        required
         label="Email"
         variant="outlined"
         inputRef={emailRef}
         onKeyPress={(e) => handleKeyPress(e, phoneRef)}
+        // I could also add validation with MUI but prefered to do with JS
+        error={errorEmail}
+        helperText={errorEmail ? emailHelperText : ""}
       />
       {/* Phone Input */}
       <TextField
+        required
         label="phone"
         variant="outlined"
         inputRef={phoneRef}
         onKeyPress={(e) => handleKeyPress(e, firstNameRef)}
+        error={errorPhone}
+        helperText={errorPhone ? phoneHelperText : ""}
       />
 
-      <Button variant="contained">Submit</Button>
-    </div>
+      <Button type="submit" variant="contained">
+        Submit
+      </Button>
+    </form>
   );
 }
