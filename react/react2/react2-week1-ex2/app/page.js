5f8ea34f1c38e283d545603aaf3704df9d8cd55d
FurkannOzbek <furkannozbek@yandex.com>
2024-09-30 14:14:19 +0200

    navbar added to layoutjs

--- react/react2/react2-week1-ex2/app/page.js
+++ react/react2/react2-week1-ex2/app/page.js
@@ -1,12 +1,3 @@
-import Image from "next/image";
-import styles from "./page.module.css";
-import Navbar from "./components/Navbar";
-
 export default function Home() {
-  return (
-    <div>
-      {" "}
-      <Navbar />
-    </div>
-  );
+  return <div></div>;
 }
